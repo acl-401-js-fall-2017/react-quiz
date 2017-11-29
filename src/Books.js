d44c4d9e427c63cb3983ada3b900e53a92f0b0e2
mirrorapps <eli.speigel@gmail.com>
2017-11-29 14:08:04 -0800

    handle add

--- src/Books.js
+++ src/Books.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Book from './Book';
+import AddBook from '../AddBook';
 export class Books extends Component {
   constructor() {
     super();
     this.state = {
       books: {}
     };
+  }
+
+  handleAdd = book => {
+    this.props.addBook(book);
   }
 
   render() {
@@ -17,9 +22,10 @@ export class Books extends Component {
               <th>Title</th>
               <th>Author</th>
             </tr>
-            {this.state.books.map((book, i) => <Book book={book} i={i}/>)}
+            {this.state.books.map((book, i) => <Book book={book} i={i} />)}
           </thead>
         </table>
+        <AddBook onComplete={this.handleAdd}/>
       </div>
     );
   }
