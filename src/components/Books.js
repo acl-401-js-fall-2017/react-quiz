ed38682c4cc3c9f8ea470ea3f8e2b17268f35af7
Zac-L <Zachlichens@gmail.com>
2017-11-29 14:18:30 -0800

    added constructor and Book view into this overall Books view

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,39 @@
 import React, { PureComponent } from 'react';
 
+import BookData from '../data/bookData';
+import Book from './Book';
+
+import { addBook } from './actions';
+
 
 export default class Books extends PureComponent {
-    state = {
-        books: []
+  constructor() {
+    super();
+    this.state = {
+      books: BookData,
+    };
+  }
+
+    handleAdd = book => {
+      const newState = addBook(this.state, book);
+      this.setState(newState);
     }
 
     render(){
-        const { books } = this.state;
+    //   const displayBooks = {
+    //     book: <Book books={this.state.books}
+    //       addBook={book => this.handleAdd(book)}
+    //     />
+    //   };
+      const { books } = this.state;
 
-        return (
-            <section>
-                <h1>All the books</h1>
-            </section>
-        )
+      return (
+        <section>
+          <h1>All the books</h1>
+          <Book books={this.state.books}
+            addBook={book => this.handleAdd(book)}
+          />
+        </section>
+      );
     }
 }
