7766a512c89002736785c19c10618809ed35ffaf
Zac-L <Zachlichens@gmail.com>
2017-11-29 14:18:04 -0800

    created some basic bookdata

---
+++ src/data/bookData.js
@@ -0,0 +1,16 @@
+const books = [
+  {
+    title: 'Book of eli',
+    author: 'Denzel'
+  },
+  {
+    title: 'Bible',
+    author: 'some entity'
+  },
+  {
+    title: 'React',
+    author: 'dkfjsdjlfk'
+  }
+];
+  
+export default books;
