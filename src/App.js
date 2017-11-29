240190b1431d8f26df913114748d55c087ddb9ae
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-29 14:28:13 -0800

    added start to add books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Books from './components/books';
+import AddBooks from './components/addBook';
 
 class App extends Component {
   render() {
@@ -9,9 +10,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Quiz</h1>
         </header>
         <Books/>
+        <AddBooks/>
       </div>
     );
   }
