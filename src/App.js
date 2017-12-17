5d07e0e9c1f7f1aa13f2dfe685aae9f686f7c2bf
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-16 17:48:10 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Books from './components/books';
-import AddBooks from './components/addBook';
 
 class App extends Component {
   render() {
@@ -10,10 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Quiz</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Books/>
-        <AddBooks/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
